bb3acb5d3f0557acf111d593c711bd9b95fa7323
EmilioZZ0 <emilio.castro@ug.uchile.cl>
2021-10-27 23:54:24 -0300

    parte enemigos

--- proyecto/scenes/Enemigo(RigBod).gd
+++ proyecto/scenes/Enemigo(RigBod).gd
@@ -4,15 +4,17 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
-
+onready var AreaVision = get_node("Area2D")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect("body_entered", self, "_on_body_entered")
+	
+	AreaVision.connect("body_entered", self, "_on_body_entered")
 
 func _on_body_entered(body: Node):
 	print(body.name)
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
