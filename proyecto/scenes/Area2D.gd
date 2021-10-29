bf08f93c4244a8bdeae304d819e0f84e61b24d5b
demiandimi <demetrio.rojas@ug.uchile.cl>
2021-10-29 00:44:25 -0300

    agregue regeneracion pasiva

--- proyecto/scenes/Area2D.gd
+++ proyecto/scenes/Area2D.gd
@@ -1,9 +1,18 @@
 extends Area2D
 onready var health = get_node("../../HUD/Control/HealthBar")
-
+#onready var invulnerable= get_node("../Invulnerable")
+#var originalhealth=health
 func _ready():
 	connect("body_entered", self, "on_body_entered")
+	#connect("timeout",self,"_on_timer_timeout")
 	
 func on_body_entered(body):
 	health.value-=5
-	print (health)
+	print (health.value)
+
+#func _on_timer_timeout():
+#	if originalhealth.value!=health.value and invulnerable.is_stopped():
+#		originalhealth.value=health.value
+#		invulnerable.start()
+#		print (originalhealth.value)
+		
--- proyecto/scenes/Control.gd
+++ proyecto/scenes/Control.gd
@@ -1,8 +1,15 @@
 extends Control
 onready var health_bar = $HealthBar
 onready var Health = get_node("../../Player/Area2D").health
+var aux=0
 func _process(_delta):
+	aux+=1
+	if aux%50==0:
+		health_bar.value+=1
+		aux=0
 	if health_bar.value != Health.value:
 		health_bar=Health.value
 		print (Health)
+	#if health_bar.value==0:
+		
 #
