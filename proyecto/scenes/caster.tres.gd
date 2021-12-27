84eb7d3f9991adc1aea99cd7d94373ea05130c59
demiandimi <demetrio.rojas@ug.uchile.cl>
2021-12-27 18:16:26 -0300

    caster

---
+++ proyecto/scenes/caster.tres.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+var vida_enemigo = 60
+var bullet = preload("res://scenes/bullet.tscn")
+
+	
+func _ready():
+	$Timer.connect("timeout", self, "_fire")
+		
+	
+
+	
+func _fire():
+	var Bullet=bullet.instance()
+	get_tree().get_root().add_child(Bullet)
+	Bullet.global_position=$bulletspawn.global_position
+
+func quitar_vida(value):
+	vida_enemigo -= value
+	print(vida_enemigo)
+
+
